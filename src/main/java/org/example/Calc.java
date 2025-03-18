31f025103f3b2f1108c9263d33e4d618ccba037d
tmfajddl <tmfajddl12@gmail.com>
2025-03-18 17:14:59 +0900

    TDD 3단계

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -4,25 +4,34 @@ public class Calc {
 
     public static int run(String exp) {
         exp = exp.replace("- ", "+ -");
-        exp = exp.replace("/ ", "* 1/");
-
-        if(exp.contains("+") || exp.contains("-")) {
-            String[] bits = exp.split(" \\+ ");
-            int a = 0;
-
-                for(int i = 0; i < bits.length; i++) {
-                    a += Integer.parseInt(bits[i]);
+        String[] bits = exp.split(" \\+ | \\* ");
+        if(exp.contains("*")) {
+            if(exp.contains("+") || exp.contains("-")){
+                int b= 1;
+                for(int i = 1; i < bits.length; i++)
+                    b *= Integer.parseInt(bits[i]);
+                int a = Integer.parseInt(bits[0]);
+                return a + b;
             }
-            return a;
-        }
-        if(exp.contains("*")||exp.contains("/")) {
-            String[] bits = exp.split(" \\* ");
-            int a = 1;
+            else{
+                int a = 1;
 
                 for(int i = 0; i < bits.length; i++) {
                     a *= Integer.parseInt(bits[i]);
+                }
+                return a;
+
             }
-            return a;
+        }
+        else if(exp.contains("+") || exp.contains("-")) {
+                int a = 0;
+
+                for(int i = 0; i < bits.length; i++) {
+                    a += Integer.parseInt(bits[i]);
+                }
+                return a;
+
+
         }
         throw new RuntimeException("해석 불가 : 올바른 계산식이 아닙니다");
     }
